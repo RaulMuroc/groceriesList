f8f4759c248cb5af25b475d9a2982c45ac6f825b
RaulMuroc <raulrodriguezcabestany@gmail.com>
2023-07-03 17:22:40 +0000

    ddd, Jul 7/3/2023, YYYY, 5:22 A +0

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app.secret_key = "b_itu!_'Ggmñp"
 
 @app.route('/')
 def index():
-    db = get_db()
-    return render_template("index.html", the_data=db)
+    the_data, shopping_list = get_db()
+    return render_template("index.html", the_data=the_data, shopping_list=shopping_list)
 
 @app.route("/add_items", methods=["post"])
 def add_items():
